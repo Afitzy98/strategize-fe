15cb4193281f4ae1216c1c9b97c9eb5b3ce15046
Aaron Fitzpatrick <a.fitzy98@gmail.com>
2021-02-08 13:33:43 +0000

    terms and conditions mostly complete

---
+++ src/pages/Terms.vue
@@ -0,0 +1,38 @@
+<template>
+  <q-page>
+    <div class="terms">
+      <h3>{{ header }}</h3>
+
+      <text-section
+        v-for="section in sections"
+        :key="section.title"
+        :title="section.title"
+        :lines="section.lines"
+      />
+    </div>
+  </q-page>
+</template>
+
+<script lang="js">
+import TextSection from 'src/components/TextSection.vue'
+import { terms } from 'src/text.json'
+
+export default {
+  name: 'PageTerms',
+  components: { TextSection },
+  data() {
+    return {
+      header: terms.header,
+      sections: terms.sections
+    }
+  }
+}
+</script>
+<style lang="scss">
+.terms {
+  padding: 1rem;
+  margin: 1rem auto;
+  color: #d3d3d3;
+  max-width: 960px;
+}
+</style>
